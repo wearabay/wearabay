03b6cd84488580a5a6cbbbd0ce05c5d1c16aae33
wearabay <wearing.abaya99@gmail.com>
2026-09-22 14:44:29 +0000

    admin: use application shell layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,12 @@
+import AdminShell from "@/components/admin/AdminShell";
 export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-full">
+    <AdminShell>
       {children}
-    </div>
+    </AdminShell>
   );
 }
